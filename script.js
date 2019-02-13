df447a314ad1f0184000d5ad7a49e7433a82d3bf
careglio <47613494+careglio@users.noreply.github.com>
2019-02-13 23:03:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function onSignIn(googleUser)
+{
+	var profile=googleUser.getBasicProfile();
+	$(".g-signin2").css("display","none");
+	$(".data").css("display","block");
+	$("#pic").attr('src',profile.getImageUrl());
+	$("#name").text(profile.getName());
+	$("#email").text(profile.getEmail());
+}
+
+function signOut()
+{
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function(){
+		alert("You signed out");
+		$(".g-signin2").css("display","block");
+		$(".data").css("display","none");
+	});
+	
+}
+	
